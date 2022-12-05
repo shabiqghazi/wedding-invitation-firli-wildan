ba45fb6d88d2c7cca6f8141cd66da382823a84bb
shabiqghazi <sgarkaan02@gmail.com>
2022-12-05 08:51:08 +0700

    fix bug in copy button

--- assets/js/tujuan.js
+++ assets/js/tujuan.js
@@ -9,6 +9,7 @@ document.getElementById("tujuan").addEventListener("keyup", () => {
   url = "https://wedding-invitation-firli-wildan.vercel.app/?u=";
   url += encodeURI(document.getElementById("tujuan").value);
   document.getElementById("url").value = url;
+  document.getElementById("copy-url").innerText = "Copy";
 });
 
 document.getElementById("copy-url").addEventListener("click", (e) => {
